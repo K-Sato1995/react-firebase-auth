f325d5bb7ccd79ce4f41c89b46b7181b1769cb56
Katsuki <Katsuki_Sato0130@icloud.com>
2020-08-25 17:44:48 +0900

    add functions

---
+++ functions/src/index.ts
@@ -0,0 +1,31 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+const firestore = admin.firestore();
+
+export const authOnCreate = functions.auth.user().onCreate(async (user) => {
+  console.log(`Creating document for user ${user.uid}`);
+  await firestore.collection("users").doc(user.uid).set({
+    createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    credits: 5,
+  });
+});
+
+export const createNewUser = functions.https.onCall(async (data, _) => {
+  try {
+    const user = await admin.auth().createUser({
+      email: data.email,
+      emailVerified: false,
+      password: data.password,
+      displayName: data.name,
+    });
+
+    if (user) {
+      await admin.auth().setCustomUserClaims(user.uid, { trustedUser: true });
+    }
+    return { status: "ok" };
+  } catch (e) {
+    throw new functions.https.HttpsError("invalid-argument", e.code);
+  }
+});
--- src/base.js
+++ src/base.js
@@ -1,5 +1,4 @@
-import * as firebase from "firebase/app";
-import "firebase/auth";
+import firebase from "firebase";
 
 export const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -7,5 +6,13 @@ export const app = firebase.initializeApp({
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+});
+
+const createNewUser = firebase.functions().httpsCallable("createNewUser");
+
+console.log(createNewUser);
+createNewUser({
+  email: "TssssS@example.com",
+  password: "111111",
 });
